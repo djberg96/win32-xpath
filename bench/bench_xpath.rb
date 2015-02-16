3bb58a4881251a07ba5ece4405d646236f88b80b
Daniel Berger <djberg96@gmail.com>
2015-02-16 08:21:31 -0700

    Added initial benchmark suite.

---
+++ bench/bench_xpath.rb
@@ -0,0 +1,16 @@
+require 'benchmark'
+require 'file'
+
+MAX = 100000
+
+Benchmark.bm(30) do |x|
+  x.report("expand_path('C:/foo/bar')") do
+    str = "C:/foo/bar"
+    MAX.times{ File.expand_path(str) }
+  end
+
+  x.report("xpath('C:/foo/bar')") do
+    str = "C:/foo/bar"
+    MAX.times{ File.xpath(str) }
+  end
+end
