98f6a1d3a9bf7101108f8efdbfafd1190c1ad8ea
Daniel Berger <djberg96@gmail.com>
2015-02-16 08:25:15 -0700

    Added some benchmarks.

--- bench/bench_xpath.rb
+++ bench/bench_xpath.rb
@@ -4,13 +4,35 @@ require 'file'
 MAX = 100000
 
 Benchmark.bm(30) do |x|
+  x.report("expand_path('foo/bar')") do
+    str = "foo/bar"
+    MAX.times{ File.expand_path(str) }
+  end
+
   x.report("expand_path('C:/foo/bar')") do
     str = "C:/foo/bar"
     MAX.times{ File.expand_path(str) }
   end
 
+  x.report("expand_path('//foo/bar')") do
+    str = "//foo/bar"
+    MAX.times{ File.expand_path(str) }
+  end
+
+##############################################
+
+  x.report("expand_path('foo/bar')") do
+    str = "foo/bar"
+    MAX.times{ File.xpath(str) }
+  end
+
   x.report("xpath('C:/foo/bar')") do
     str = "C:/foo/bar"
+    MAX.times{ File.xpath(str) }
+  end
+
+  x.report("xpath('//foo/bar')") do
+    str = "//foo/bar"
     MAX.times{ File.xpath(str) }
   end
 end
