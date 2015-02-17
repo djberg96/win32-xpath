46b9456372c974ff17fa5c27a42cc5d3d6c0bdd7
Daniel Berger <djberg96@gmail.com>
2015-02-17 06:27:25 -0700

    Remove unicode for now, strip trailing slashes.

--- ext/xpath.c
+++ ext/xpath.c
@@ -1,18 +1,28 @@
-#define UNICODE
-#define _UNICODE
-
 #include <ruby.h>
 #include <windows.h>
+#include <shlwapi.h>
 
 static VALUE rb_xpath(int argc, VALUE* argv, VALUE self){
   VALUE v_path, v_dir;
+  TCHAR* path;
 
   rb_scan_args(argc, argv, "11", &v_path, &v_dir);
 
+  if (rb_respond_to(v_path, rb_intern("to_path")))
+    v_path = rb_funcall(v_path, rb_intern("to_path"), 0, NULL);
+
   Check_Type(v_path, T_STRING);
 
-  if(!NIL_P(v_dir))
+  if (!NIL_P(v_dir))
     Check_Type(v_dir, T_STRING);
+
+  v_path = rb_funcall(v_path, rb_intern("tr"), 2, rb_str_new2("/"), rb_str_new2("\\"));
+
+  path = StringValuePtr(v_path);
+
+  while (!*PathRemoveBackslash(path));
+
+  printf("Path is: %s\n", path);
 
   return self;
 }
