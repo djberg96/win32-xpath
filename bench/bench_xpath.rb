29dd61a7a290f5f5b1d7158310d9f7998583a4af
Daniel Berger <djberg96@gmail.com>
2015-02-17 11:40:37 -0700

    Reordered benchmarks.

--- bench/bench_xpath.rb
+++ bench/bench_xpath.rb
@@ -9,31 +9,14 @@ Benchmark.bm(30) do |x|
     MAX.times{ File.expand_path(str) }
   end
 
-  x.report("expand_path('C:/foo/bar')") do
-    str = "C:/foo/bar"
-    MAX.times{ File.expand_path(str) }
-  end
-
-  x.report("expand_path('//foo/bar')") do
-    str = "//foo/bar"
-    MAX.times{ File.expand_path(str) }
-  end
-
-  x.report("expand_path('foo//bar///')") do
-    str = "foo//bar///"
-    MAX.times{ File.expand_path(str) }
-  end
-
-  x.report("expand_path('')") do
-    str = ""
-    MAX.times{ File.expand_path(str) }
-  end
-
-##############################################
-
   x.report("xpath('foo/bar')") do
     str = "foo/bar"
     MAX.times{ File.xpath(str) }
+  end
+
+  x.report("expand_path('C:/foo/bar')") do
+    str = "C:/foo/bar"
+    MAX.times{ File.expand_path(str) }
   end
 
   x.report("xpath('C:/foo/bar')") do
@@ -41,14 +24,39 @@ Benchmark.bm(30) do |x|
     MAX.times{ File.xpath(str) }
   end
 
+  x.report("expand_path('//foo/bar')") do
+    str = "//foo/bar"
+    MAX.times{ File.expand_path(str) }
+  end
+
   x.report("xpath('//foo/bar')") do
     str = "//foo/bar"
     MAX.times{ File.xpath(str) }
   end
 
+  x.report("expand_path('foo//bar///')") do
+    str = "foo//bar///"
+    MAX.times{ File.expand_path(str) }
+  end
+
   x.report("xpath('foo//bar///')") do
     str = "foo//bar///"
     MAX.times{ File.xpath(str) }
+  end
+
+  x.report("expand_path('~')") do
+    str = "~"
+    MAX.times{ File.expand_path(str) }
+  end
+
+  x.report("xpath('~')") do
+    str = "~"
+    MAX.times{ File.xpath(str) }
+  end
+
+  x.report("expand_path('')") do
+    str = ""
+    MAX.times{ File.expand_path(str) }
   end
 
   x.report("xpath('')") do
