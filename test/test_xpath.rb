59a75a4e14f5ef1e947b56266430394cbb20ff96
Daniel Berger <djberg96@gmail.com>
2015-02-26 11:33:10 -0700

    Change xpath to expand_path.

--- test/test_xpath.rb
+++ test/test_xpath.rb
@@ -19,153 +19,153 @@ class Test_XPath < Test::Unit::TestCase
   end
 
   test "converts an empty pathname into absolute current pathname" do
-    assert_equal(@pwd, File.xpath(''))
+    assert_equal(@pwd, File.expand_path(''))
   end
 
   test "converts '.' into absolute pathname using current directory" do
-    assert_equal(@pwd, File.xpath('.'))
+    assert_equal(@pwd, File.expand_path('.'))
   end
 
   test "converts 'foo' into absolute pathname using current directory" do
-    assert_equal(File.join(@pwd, 'foo'), File.xpath('foo'))
+    assert_equal(File.join(@pwd, 'foo'), File.expand_path('foo'))
   end
 
   test "converts 'foo' into absolute pathname ignoring nil dir" do
-    assert_equal(File.join(@pwd, 'foo'), File.xpath('foo', nil))
+    assert_equal(File.join(@pwd, 'foo'), File.expand_path('foo', nil))
   end
 
   test "converts 'foo' and 'bar' into absolute pathname" do
-    assert_equal(File.join(@pwd, "bar", "foo"), File.xpath('foo', 'bar'))
+    assert_equal(File.join(@pwd, "bar", "foo"), File.expand_path('foo', 'bar'))
   end
 
   test "converts a pathname into absolute pathname" do
-    assert_equal(File.join(@pwd, 'a'), File.xpath('a.'))
-    assert_equal(File.join(@pwd, '.a'), File.xpath('.a'))
-    assert_equal(File.join(@pwd, '..a'), File.xpath('..a'))
-    assert_equal(File.join(@pwd, 'a../b'), File.xpath('a../b'))
+    assert_equal(File.join(@pwd, 'a'), File.expand_path('a.'))
+    assert_equal(File.join(@pwd, '.a'), File.expand_path('.a'))
+    assert_equal(File.join(@pwd, '..a'), File.expand_path('..a'))
+    assert_equal(File.join(@pwd, 'a../b'), File.expand_path('a../b'))
   end
 
   test "converts a pathname and make it valid" do
-    assert_equal(File.join(@pwd, 'a'), File.xpath('a..'))
+    assert_equal(File.join(@pwd, 'a'), File.expand_path('a..'))
   end
 
   test "converts a pathname to an absolute pathname using a complete path" do
-    assert_equal(@tmp, File.xpath('', @tmp))
-    assert_equal(File.join(@tmp, 'a'), File.xpath('a', @tmp))
-    assert_equal(File.join(@tmp, 'a'), File.xpath('../a', "#{@tmp}/xxx"))
-    assert_equal(@root, File.xpath('.', @root))
+    assert_equal(@tmp, File.expand_path('', @tmp))
+    assert_equal(File.join(@tmp, 'a'), File.expand_path('a', @tmp))
+    assert_equal(File.join(@tmp, 'a'), File.expand_path('../a', "#{@tmp}/xxx"))
+    assert_equal(@root, File.expand_path('.', @root))
   end
 
   test "ignores supplied dir if path contains a drive letter" do
-    assert_equal(@root, File.xpath(@root, "D:/"))
+    assert_equal(@root, File.expand_path(@root, "D:/"))
   end
 
   test "removes trailing slashes from absolute path" do
-    assert_equal(File.join(@root, 'foo'), File.xpath("#{@root}foo/"))
-    assert_equal(File.join(@root, 'foo.rb'), File.xpath("#{@root}foo.rb/"))
+    assert_equal(File.join(@root, 'foo'), File.expand_path("#{@root}foo/"))
+    assert_equal(File.join(@root, 'foo.rb'), File.expand_path("#{@root}foo.rb/"))
   end
 
   test "removes trailing spaces from absolute path" do
-    assert_equal(File.join(@root, 'foo'), File.xpath("#{@root}foo  "))
+    assert_equal(File.join(@root, 'foo'), File.expand_path("#{@root}foo  "))
   end
 
   test "removes trailing dots from absolute path" do
-    assert_equal(File.join(@root, 'a'), File.xpath("#{@root}a."))
+    assert_equal(File.join(@root, 'a'), File.expand_path("#{@root}a."))
   end
 
   test "converts a pathname with a drive letter but no slash" do
-    assert_match(/\Ac:\//i, File.xpath("c:"))
+    assert_match(/\Ac:\//i, File.expand_path("c:"))
   end
 
   test "converts a pathname with a drive letter ignoring different drive dir" do
-    assert_match(/\Ac:\//i, File.xpath("c:foo", "d:/bar"))
+    assert_match(/\Ac:\//i, File.expand_path("c:foo", "d:/bar"))
   end
 
   test "converts a pathname which starts with a slash using current drive" do
-    assert_match(/\A#{@drive}\/foo\z/i, File.xpath('/foo'))
+    assert_match(/\A#{@drive}\/foo\z/i, File.expand_path('/foo'))
   end
 
   test "returns tainted strings or not" do
-    assert_true(File.xpath('foo').tainted?)
-    assert_true(File.xpath('foo'.taint).tainted?)
-    assert_true(File.xpath('/foo').tainted?)
-    assert_true(File.xpath('/foo'.taint).tainted?)
-    assert_true(File.xpath('C:/foo'.taint).tainted?)
-    assert_false(File.xpath('C:/foo').tainted?)
-    assert_false(File.xpath('//foo').tainted?)
+    assert_true(File.expand_path('foo').tainted?)
+    assert_true(File.expand_path('foo'.taint).tainted?)
+    assert_true(File.expand_path('/foo').tainted?)
+    assert_true(File.expand_path('/foo'.taint).tainted?)
+    assert_true(File.expand_path('C:/foo'.taint).tainted?)
+    assert_false(File.expand_path('C:/foo').tainted?)
+    assert_false(File.expand_path('//foo').tainted?)
   end
 
   test "converts a pathname to an absolute pathname using tilde as base" do
-    assert_equal(@home, File.xpath('~'))
-    assert_equal("#{@home}/foo", File.xpath('~/foo'))
+    assert_equal(@home, File.expand_path('~'))
+    assert_equal("#{@home}/foo", File.expand_path('~/foo'))
   end
 
   test "converts a pathname to an absolute pathname using tilde for UNC path" do
     ENV['HOME'] = @unc
-    assert_equal(@unc, File.xpath('~'))
+    assert_equal(@unc, File.expand_path('~'))
   end
 
   test "converts a tilde to path if used for dir argument" do
-    assert_equal(@home, File.xpath('', '~'))
-    assert_equal("#{@home}/foo", File.xpath('', '~/foo'))
-    assert_equal("#{@home}/foo", File.xpath('foo', '~'))
+    assert_equal(@home, File.expand_path('', '~'))
+    assert_equal("#{@home}/foo", File.expand_path('', '~/foo'))
+    assert_equal("#{@home}/foo", File.expand_path('foo', '~'))
   end
 
   test "does not modify a HOME string argument" do
     str = "~/a"
-    assert_equal("#{@home}/a", File.xpath(str))
+    assert_equal("#{@home}/a", File.expand_path(str))
     assert_equal("~/a", str)
   end
 
   test "raises ArgumentError when HOME is nil" do
     ENV['HOME'] = nil
     ENV['USERPROFILE'] = nil
-    assert_raise(ArgumentError){ File.xpath('~') }
+    assert_raise(ArgumentError){ File.expand_path('~') }
   end
 
   test "raises ArgumentError if HOME is relative" do
     ENV['HOME'] = '.'
-    assert_raise(ArgumentError){ File.xpath('~') }
+    assert_raise(ArgumentError){ File.expand_path('~') }
   end
 
   test "raises ArgumentError if relative user is provided" do
     ENV['HOME'] = '.'
-    assert_raise(ArgumentError){ File.xpath('~anything') }
+    assert_raise(ArgumentError){ File.expand_path('~anything') }
   end
 
   test "raises an ArgumentError if a bogus username is supplied" do
-    assert_raise(ArgumentError){ File.xpath('~anything') }
+    assert_raise(ArgumentError){ File.expand_path('~anything') }
   end
 
   test "converts a tilde plus username as expected" do
-    assert_equal("C:/Users/#{@@login}", File.xpath("~#{@@login}"))
-    assert_equal("C:/Users/#{@@login}/foo", File.xpath("~#{@@login}/foo"))
+    assert_equal("C:/Users/#{@@login}", File.expand_path("~#{@@login}"))
+    assert_equal("C:/Users/#{@@login}/foo", File.expand_path("~#{@@login}/foo"))
   end
 
   test "raises a TypeError if not passed a string" do
-    assert_raise(TypeError){ File.xpath(1) }
-    assert_raise(TypeError){ File.xpath(nil) }
-    assert_raise(TypeError){ File.xpath(true) }
+    assert_raise(TypeError){ File.expand_path(1) }
+    assert_raise(TypeError){ File.expand_path(nil) }
+    assert_raise(TypeError){ File.expand_path(true) }
   end
 
   test "canonicalizes absolute path" do
-    assert_equal("C:/dir", File.xpath("C:/./dir"))
+    assert_equal("C:/dir", File.expand_path("C:/./dir"))
   end
 
   test "does not modify its argument" do
     str = "./a/b/../c"
-    assert_equal("#{@home}/a/c", File.xpath(str, @home))
+    assert_equal("#{@home}/a/c", File.expand_path(str, @home))
     assert_equal("./a/b/../c", str)
   end
 
   test "accepts objects that have a to_path method" do
     klass = Class.new{ def to_path; "a/b/c"; end }
     obj = klass.new
-    assert_equal("#{@pwd}/a/b/c", File.xpath(obj))
+    assert_equal("#{@pwd}/a/b/c", File.expand_path(obj))
   end
 
   test "works with unicode characters" do
-    assert_equal("#{@pwd}/Ελλάσ", File.xpath("Ελλάσ"))
+    assert_equal("#{@pwd}/Ελλάσ", File.expand_path("Ελλάσ"))
   end
 
   def teardown
