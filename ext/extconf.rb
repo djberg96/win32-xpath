5da897bac56ed1cb369393f4d4115033c73cc924
Daniel Berger <djberg96@gmail.com>
2015-06-24 21:41:14 -0600

    Check for pathcch lib and header.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,13 @@
 require 'mkmf'
+
 have_library('shlwapi')
 have_library('advapi32')
+
+# Windows 8 or later
+if have_library('pathcch')
+  if have_header('pathcch.h')
+    have_func('PathCchAppendEx', 'pathcch.h')
+  end
+end
+
 create_makefile('win32/xpath', 'win32')
