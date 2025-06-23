54edb07e5e6816ac0019e3e382c9acd1eaa87b13
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-06-23 06:18:11 -0400

    Remove inline check.

--- lib/win32/xpath.rb
+++ lib/win32/xpath.rb
@@ -24,13 +24,13 @@ class File
     # Handle type checking and conversion
     original_path = path
     path = path.to_path if path.respond_to?(:to_path)
-    
+
     unless path.is_a?(String)
       raise TypeError, "no implicit conversion of #{path.class} into String"
     end
-    
+
     path = path.dup
-    
+
     # Handle dir argument
     if dir
       dir = dir.to_path if dir.respond_to?(:to_path)
@@ -63,7 +63,7 @@ class File
     # Use Windows API to get full path - allocate more space for very long paths
     buffer_size = [path.length * 2, MAX_PATH * 4, 32768].max
     buffer = FFI::MemoryPointer.new(:char, buffer_size)
-    
+
     result = GetFullPathName(path, buffer_size, buffer, nil)
     if result == 0
       # If GetFullPathName fails, try manual construction for very long paths
@@ -79,10 +79,10 @@ class File
     else
       result_path = formatted_windows_string(buffer)
     end
-    
+
     # Ensure UTF-8 encoding to match Ruby standards
     result_path.force_encoding('UTF-8') if result_path.respond_to?(:force_encoding)
-    
+
     result_path
   end
 
@@ -104,36 +104,36 @@ class File
         user = parts[0] || ''
         rest = parts[1] ? '/' + parts[1] : ''
       end
-      
+
       if user.empty?
         home = get_home_directory
       else
         home = get_user_home_directory(user)
       end
-      
+
       return home + rest
     end
-    
+
     path
   end
 
   def self.get_home_directory
     home = ENV['HOME'] || ENV['USERPROFILE']
-    
+
     if home.nil? && ENV['HOMEDRIVE'] && ENV['HOMEPATH']
       home = ENV['HOMEDRIVE'] + ENV['HOMEPATH']
     end
-    
+
     if home.nil?
       raise ArgumentError, "couldn't find HOME environment -- expanding '~'"
     end
-    
+
     home = home.tr('\\', '/')
-    
+
     unless absolute_path?(home)
       raise ArgumentError, "non-absolute home"
     end
-    
+
     home
   end
   def self.get_user_home_directory(username)
@@ -143,55 +143,55 @@ class File
     sid_size = FFI::MemoryPointer.new(:ulong, 1)
     domain_size = FFI::MemoryPointer.new(:ulong, 1)
     sid_name_use = FFI::MemoryPointer.new(:int, 1)
-    
+
     sid_size.write_ulong(256)
     domain_size.write_ulong(256)
-    
+
     # Look up the user account
     success = LookupAccountName(nil, username, sid_buffer, sid_size, domain_buffer, domain_size, sid_name_use)
-    
+
     unless success
       raise ArgumentError, "can't find user '#{username}'"
     end
-    
+
     # Convert SID to string
     string_sid_ptr = FFI::MemoryPointer.new(:pointer, 1)
     success = ConvertSidToStringSid(sid_buffer, string_sid_ptr)
-    
+
     unless success
       raise SystemCallError.new('ConvertSidToStringSid', FFI.errno)
     end
-    
+
     string_sid = string_sid_ptr.read_pointer.read_string
-    
+
     # Build registry key path
     key_path = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\#{string_sid}"    # Open registry key
     key_handle = FFI::MemoryPointer.new(:ulong, 1)
     result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, key_path, 0, KEY_QUERY_VALUE, key_handle)
-    
+
     # Free the string SID
     LocalFree(string_sid_ptr.read_pointer)
-    
+
     if result != 0
       raise Errno::ENOENT, "can't find home directory for user '#{username}'"
     end
-    
+
     handle = key_handle.read_ulong
       # Query the ProfileImagePath value
     data_buffer = FFI::MemoryPointer.new(:char, MAX_PATH * 2)
     data_size = FFI::MemoryPointer.new(:ulong, 1)
     data_type = FFI::MemoryPointer.new(:ulong, 1)
-    
+
     data_size.write_ulong(MAX_PATH * 2)
-    
+
     result = RegQueryValueEx(handle, "ProfileImagePath", nil, data_type, data_buffer, data_size)
-    
+
     RegCloseKey(handle)
-    
+
     if result != 0
       raise Errno::ENOENT, "can't find home directory for user '#{username}'"
     end
-    
+
     home = data_buffer.read_string.tr('\\', '/')
     home
   end
@@ -201,31 +201,23 @@ class File
   end
   def self.formatted_windows_string(buffer)
     str = buffer.read_string.tr('\\', '/')
-    
+
     # Remove trailing slashes except for root paths
     while str.length > 1 && str[-1] == '/' && !(str =~ /^[a-zA-Z]:\/$/i)
       str = str[0..-2]
     end
-    
+
     # Ensure root drives end with slash
     if str =~ /^[a-zA-Z]:$/i
       str += '/'
     end
-    
+
     # Handle Windows path quirks - remove trailing spaces and dots
     str = str.rstrip
     while str.length > 1 && str[-1] == '.' && str[-2] != '/'
       str = str[0..-2]
     end
-    
+
     str
   end
 end
-
-=begin
-if $0 == __FILE__
-  p File.expand_path('.')
-  p File.expand_path('foo')
-  p File.expand_path('foo/bar.txt')
-end
-=end
